739a5ac41d01ea608b4e4d630a0c6f1db460a24c
urataku1217 <urataku.com@gmail.com>
2020-05-31 14:58:08 +0900

    test the action that creates a message in the messages controller

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -4,7 +4,7 @@ describe MessagesController do
   let (:group) { create(:group) }
   let (:user) { create(:user) }
 
-  describe 'GET #index' do
+  describe '#index' do
     
     context 'ログインしている場合' do
       before do
@@ -31,6 +31,58 @@ describe MessagesController do
       end
 
       it 'ログイン画面にリダイレクトすること' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe '#create' do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+    
+    context 'ログインしている場合' do
+      before do
+        login user
+      end
+
+      context '保存に成功した場合' do
+        subject {
+          post :create,
+          params: params
+        }
+        
+        it 'messageを保存すること' do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+  
+        it 'group_messages_pathにリダイレクトすること' do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+      context '保存に失敗した場合' do
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, content: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it 'messageを保存しないこと' do
+          expect{ subject }.not_to change(Message, :count)
+        end
+  
+        it 'index.html.hamlに遷移すること' do
+          subject
+          expect(response).to render_template :index
+        end
+      end
+    end
+    
+    context 'ログインしていない場合' do
+      
+      it 'ログイン画面にリダイレクトすること' do
+        post :create, params: params
         expect(response).to redirect_to(new_user_session_path)
       end
     end
