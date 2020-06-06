be9147dbf4e6a162c1f49f95f01df03716016437
urataku1217 <urataku.com@gmail.com>
2020-06-06 19:11:21 +0900

    receive json with done method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,44 @@
 $(function() {
 
+  function buildHTML(message){
+    if (message.image) {
+      var html = `<div class="message">
+                    <div class="message__upper-info">
+                      <p class="message__upper-info__talker">
+                        ${message.user.name}
+                      </p>
+                      <p class="message__upper-info__date">
+                        ${message.created_at}
+                      </p>
+                    </div>
+                    <div class="message-text">
+                      <p class="message-text__content">
+                        ${message.content}
+                      </p>
+                      <img class="message-text__image" src="${message.image}">
+                    </div>
+                  </div>`
+      return html;
+    } else {
+      var html = `<div class="message">
+                    <div class="message__upper-info">
+                      <p class="message__upper-info__talker">
+                        ${message.user.name}
+                      </p>
+                      <p class="message__upper-info__date">
+                        ${message.created_at}
+                      </p>
+                    </div>
+                    <div class="message-text">
+                      <p class="message-text__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,6 +50,10 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      
     })
   });
 
