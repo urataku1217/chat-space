c244e5fc0b86f63aa95492d1a1ffefc3c8046329
urataku1217 <urataku.com@gmail.com>
2020-06-08 23:19:00 +0900

    空の状態でSendボタンをクリックし、アラートが表示された後でも、リロードせずにメッセージ送信できるようにする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,10 +56,12 @@ $(function() {
       $('.messages').append(html);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('form')[0].reset();
-      $('.submit-btn').removeAttr('disabled');
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
-    });
+    })
+    .always(function(data) {
+      $('.submit-btn').prop('disabled', false);
+    })
   });
 });
